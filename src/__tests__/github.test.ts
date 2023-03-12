a776840076f8c0a0ec3b9797535d032d77af51d0
Benjamin Shafii <ben@prologe.io>
2023-03-12 09:37:44 +0100

    Show example for fetching github

--- src/__tests__/github.test.ts
+++ src/__tests__/github.test.ts
@@ -1,28 +1,30 @@
-import { getAllFilesFromGithubRepo } from '../helpers/github';
-import { writeFile } from 'fs';
-import fetch from 'cross-fetch';
+import { getAllFilesFromGithubRepo } from '../';
+// uncoment all the file to download create a files that join
+// import { writeFile } from 'fs';
+// import fetch from 'cross-fetch';
 
 const githubToken = process.env.GITHUB_TOKEN || '';
 
-test('it can retrieve all files from a github repo', async () => {
+// this is more a utils than a test
+test.skip('it can retrieve all files from a github repo', async () => {
   const files = await getAllFilesFromGithubRepo(
-    'https://api.github.com/repos/different-ai/embedbase/contents',
+    'https://api.github.com/repos/louis030195/obsidian-ava/contents',
     githubToken,
   );
-  // only keep python files
-  const pythonFiles = files.filter((file) => file.name.endsWith('.py'));
-  const content = pythonFiles.map(async (file) => {
-    return await fetch(file.download_url, {
-      headers: {
-        Authorization: `token ${githubToken}`,
-      },
-    }).then((response) => response.text());
-  });
-  const content2 = await Promise.all(content);
+  // // only keep python files
+  // const pythonFiles = files.filter((file) => file.name.endsWith('.py'));
+  // const content = pythonFiles.map(async (file) => {
+  //   return await fetch(file.download_url, {
+  //     headers: {
+  //       Authorization: `token ${githubToken}`,
+  //     },
+  //   }).then((response) => response.text());
+  // });
+  // const content2 = await Promise.all(content);
 
-  writeFile('python_files.md', content2.join(), (err) => {
-    'test';
-  });
+  // writeFile('python_files.md', content2.join(), (err) => {
+  //   'test';
+  // });
 
   expect(files.length).toBeGreaterThan(0);
 });
